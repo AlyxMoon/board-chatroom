beb01ceeee4cbfcffda4d1703f75d9f17a9cf63a
Allister Moon <allisterkmoon@gmail.com>
2023-10-13 16:36:05 -0700

    get things setup again and start using yarn also added skeleton-css, spooky season

---
+++ frontend/src/components/registrationForm.component.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="username">
+    <input type="password" v-model="password">
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
